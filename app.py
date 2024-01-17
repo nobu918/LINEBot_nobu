524f3c9f74c859317f74c820cce181ce9b97b27b
nobu <shinzi.567.wrwr@gmail.com>
2024-01-17 19:19:22 +0900

    komento

---
+++ app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+	ApiClient, Configuration, MessagingApi,
+	ReplyMessageRequest, PushMessageRequest,
+	TextMessage, PostbackAction
+)
+from linebot.v3.webhooks import (
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+)
+import os
